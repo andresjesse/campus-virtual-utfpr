cf089c4ff491c4036d32c0394fb6edbbd392b101
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-07-21 19:39:32 -0300

    FEAT: Adding guard on routes.

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import {
+  ProtectedRoute,
+  PublicOnlyRoute,
+} from "@/components/authentication-route";
 import { Pages } from "@/pages";
 
 export const Router = () => {
@@ -8,8 +12,14 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<Pages.Home />} />
         <Route path="/home" element={<Pages.Home />} />
-        <Route path="/login" element={<Pages.Login />} />
-        <Route path="/admin" element={<Pages.Admin.Home />} />
+
+        <Route element={<PublicOnlyRoute />}>
+          <Route path="/login" element={<Pages.Login />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/admin" element={<Pages.Admin.Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
