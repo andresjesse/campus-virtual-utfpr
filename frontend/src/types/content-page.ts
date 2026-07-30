3688eae1a5b164e98796d15b863114aaa46a6ad8
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-07-29 23:14:45 -0300

    CHORE: Adding types to support the content page blocks handling.

--- frontend/src/types/content-page.ts
+++ frontend/src/types/content-page.ts
@@ -1,5 +1,31 @@
 import type { RecordModel } from "pocketbase";
 
+export type RtfBlockValue = {
+  title: string;
+  content: string;
+}
+
+export type DiagramBlockValue = {
+  title: string;
+  content: string;
+}
+
+export type FileBlockValue = {
+  title: string;
+  content: string;
+}
+
+export type ContentPageFormValues = {
+  relation: string;
+  title: string;
+};
+
+export type ContentPageBlocksValues = {
+  rtfBlocks?: RtfBlockValue[];
+  diagramBlocks?: DiagramBlockValue[];
+  fileBlocks?: FileBlockValue[];
+} | undefined;
+
 export type EntityRecord = RecordModel & {
   slug: string;
 };
@@ -9,11 +35,6 @@ export type ContentPageRecord = RecordModel & {
   expand?: {
     entity?: EntityRecord;
   };
-  title: string;
-};
-
-export type ContentPageFormValues = {
-  relation: string;
   title: string;
 };
 
