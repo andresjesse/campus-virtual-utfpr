b1ffcc530ee9fa1e319d13332192c939753b9eb2
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-08-18 23:07:54 -0300

    CHORE: Adding static globals.css so the IDE won't complain about dynamic style injection performed by apply-branding.ts.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import '@mantine/tiptap/styles.css';
+import "./globals.css";
 
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
